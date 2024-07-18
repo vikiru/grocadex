7e338a0d9958966ab9046750a4fe508fc812b151
vikiru <72267229+vikiru@users.noreply.github.com>
2024-07-18 19:54:36 -0400

    Update issue template and tailwind config

--- .github/ISSUE_TEMPLATE/default.md
+++ .github/ISSUE_TEMPLATE/default.md
@@ -3,5 +3,5 @@ name: 'Default Issue'
 about: 'A template for all issues within this GitHub project'
 title: 'Default Issue'
 assignees: vikiru
-projects: ['grocery-tracker']
+projects: ["vikiru/10"]
 ---
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./app.{js,jsx,ts,tsx}'],
+    content: [
+        './app.{js,jsx,ts,tsx}',
+        './screens/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',
+        './navigation/**/*.{js,jsx,ts,tsx}',
+    ],
     theme: {
         extend: {},
     },
