265890261d18d268d14c8cecf2629384a4148fd9
vikiru <72267229+vikiru@users.noreply.github.com>
2024-08-07 18:48:25 -0400

    Update dependencies and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './app.{js,jsx,ts,tsx}',
+        './app/**/*.{js,jsx,ts,tsx}',
         './screens/**/*.{js,jsx,ts,tsx}',
         './components/**/*.{js,jsx,ts,tsx}',
         './navigation/**/*.{js,jsx,ts,tsx}',
