03113fd81abfe356f57de13bc902f656e020adf4
vikiru <72267229+vikiru@users.noreply.github.com>
2024-08-17 16:38:12 -0400

    Define Receipt model

---
+++ models/Receipt.ts
@@ -0,0 +1,48 @@
+import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model, Sequelize } from 'sequelize';
+
+import { UserModel } from './User';
+
+const sequelize = new Sequelize('sqlite::memory:');
+
+interface Receipt extends Model<InferAttributes<Receipt>, InferCreationAttributes<Receipt>> {
+    id: CreationOptional<number>;
+    userId: number;
+    store: string;
+    purchaseDate: string;
+    total: number;
+}
+
+export const ReceiptModel = sequelize.define<Receipt>(
+    'Receipt',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: UserModel,
+                key: 'id',
+            },
+        },
+        store: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        purchaseDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        total: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+    },
+    {
+        underscored: true,
+        timestamps: true,
+    },
+);
