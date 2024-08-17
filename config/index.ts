e04fe1c3f8df7ef9778263499191aff32f8cac64
vikiru <72267229+vikiru@users.noreply.github.com>
2024-08-17 17:25:41 -0400

    Define config setup with env variables

---
+++ config/index.ts
@@ -0,0 +1,20 @@
+const path = require('path');
+require('dotenv').config({
+    path: path.resolve(__dirname, '../.env'),
+});
+
+export const config = {
+    database: {
+        name: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+    },
+    email: {
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        user: process.env.EMAIL_USER,
+        password: process.env.EMAIL_PASSWORD,
+    },
+};
