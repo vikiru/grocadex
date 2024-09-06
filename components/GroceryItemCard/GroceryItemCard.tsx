91b1564a9bf621ece5f80c36d166010bf797fbc4
vikiru <72267229+vikiru@users.noreply.github.com>
2024-09-06 17:00:42 -0400

    Design initial grocery expiry screen

---
+++ components/GroceryItemCard/GroceryItemCard.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+
+import { Text, View } from 'react-native';
+import { Button, Card } from 'react-native-paper';
+
+import { StyledComponent } from 'nativewind';
+import { GroceryItem } from '../../types/GroceryItem';
+import { formatDate } from '../ReceiptCard/ReceiptCard';
+
+export default function GroceryItemCard({ item }: { item: GroceryItem | Partial<GroceryItem> }) {
+    return (
+        <StyledComponent component={Card} className="bg-white shadow-md rounded-lg p-4 m-2">
+            <Card.Content>
+                <StyledComponent component={View} className="flex flex-row justify-between">
+                    <StyledComponent component={Text} className="text-lg font-heading font-semibold">
+                        {item.name} ({item.quantity})
+                    </StyledComponent>
+                    <StyledComponent component={Text} className="text-lg font-heading font-semibold">
+                        ${item.totalPrice}
+                    </StyledComponent>
+                </StyledComponent>
+                <StyledComponent component={Text} className="text-base text-red-500">
+                    Expiring on {formatDate(item.expiryDate)}
+                </StyledComponent>
+            </Card.Content>
+            <StyledComponent component={Card.Actions} className="flex flex-row justify-between m-1">
+                <StyledComponent
+                    component={Button}
+                    icon="pencil"
+                    mode="text"
+                    className="bg-primary w-1/2"
+                    textColor="white"
+                    onPress={() => {}}
+                >
+                    Edit
+                </StyledComponent>
+                <StyledComponent
+                    component={Button}
+                    icon="delete"
+                    mode="text"
+                    className="bg-red-400 w-1/2"
+                    textColor="white"
+                    onPress={() => {}}
+                >
+                    Delete
+                </StyledComponent>
+            </StyledComponent>
+        </StyledComponent>
+    );
+}
