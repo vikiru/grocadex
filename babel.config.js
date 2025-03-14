630677e749d013a7b58d12650efaca35734cccfe
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2025-03-14 03:59:36 -0400

    Add tamagui UI

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-        plugins: ['react-native-reanimated/plugin', ['react-native-paper/babel']],
+        plugins: ['@tamagui/babel-plugin', 'react-native-reanimated/plugin', ['react-native-paper/babel']],
     };
 };
