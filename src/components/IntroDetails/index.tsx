680ecd9679cee92beeb882f3b19607b0f67c4bd9
vikiru <72267229+vikiru@users.noreply.github.com>
2025-03-19 02:33:18 -0400

    refactor: re-design splash screen

--- src/components/IntroDetails/index.tsx
+++ src/components/IntroDetails/index.tsx
@@ -12,17 +12,17 @@ function IntroDetails(props: IntroDetailsProps) {
         <View className="mx-auto my-2 flex flex-row items-center">
             <View className="flex h-16 w-16 max-w-[20%] items-center justify-center">
                 <MaterialCommunityIcons
-                    className="mx-3 text-light-text-950 dark:text-dark-text-950"
+                    className="mx-3 text-light-text-950"
                     name={props.icon}
                     size={40}
                 />
             </View>
-            <View className="ml-4 flex max-w-[300px] flex-col lg:max-w-[400px]">
-                <Text className="font-heading text-h5 font-bold text-light-text-950 dark:text-dark-text-950">
+            <View className="ml-4 flex max-w-[300px] flex-col md:max-w-[400px] xl:max-w-[600px] 4xl:max-w-[800px]">
+                <Text className="h5 font-heading font-bold text-light-text-950">
                     {props.heading}
                 </Text>
                 <View className="mr-5 flex">
-                    <Text className="font-body mr-2 mt-1 text-left text-h6 text-light-text-950 dark:text-dark-text-950">
+                    <Text className="p mr-2 mt-1 text-left text-light-text-950">
                         {props.subtext}
                     </Text>
                 </View>
