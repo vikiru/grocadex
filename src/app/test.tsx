631e44e0cbc1bd1695ef156a3aa93b85e42872a0
vikiru <72267229+vikiru@users.noreply.github.com>
2025-03-20 21:47:00 -0400

    refactor: re-design grocery card

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,4 +1,5 @@
 import { ScrollView } from 'react-native';
+import GroceryCard from '~components/GroceryCard';
 import ReceiptCard from '~components/ReceiptCard';
 import { VStack } from '~components/ui/vstack';
 
@@ -6,11 +7,10 @@ function Test() {
     return (
         <ScrollView className="mx-6 mt-2 overflow-x-hidden">
             <VStack className="grid gap-4 xs:grid-cols-1 lg:grid-cols-2">
-                <ReceiptCard />
-                <ReceiptCard />
-                <ReceiptCard />
-                <ReceiptCard />
-                <ReceiptCard />
+                <GroceryCard />
+                <GroceryCard />
+                <GroceryCard />
+                <GroceryCard />
             </VStack>
         </ScrollView>
     );
